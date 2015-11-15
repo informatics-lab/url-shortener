7b715080d60e6073789afdd743a3ac6a3e33db06
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2015-11-15 16:20:50 +0000

    Abstracted creation and sending of response to ensure consistency and remove duplicate code

--- src/routes/api.js
+++ src/routes/api.js
@@ -5,70 +5,48 @@ var db = require('../models/database')
 var settings = require('../settings/settings')
 db.connect()
 
-router.get('/', function (req, res) {
-  data = {
-    "status" : 200,
-    "result" : {
-      "message" : "Welcome to the API"
-    }
+var build_response = function(status, message, result){
+  return {
+    "status" : status,
+    "message" : message,
+    "result" : result
   }
+}
 
+var respond = function(res, data){
   res.setHeader('Content-Type', 'application/json')
+  res.status(data.status)
   res.send(JSON.stringify(data))
+}
+
+router.get('/', function (req, res) {
+  var data = build_response(200, "Welcome to the API", null)
+  respond(res, data)
 });
 
 router.get('/check/:short', function (req, res) {
   db.check_short(req.params.short, function(err, url){
     if (url){
       url.domain = settings.domain
-      var data = {
-        "status" : 200,
-        "result" : url
-      }
+      var data = build_response(200, "Short exists", url)
     } else {
-      var data = {
-        "status" : 404,
-        "result" : {
-          "message" : "Short not found"
-        }
-      }
+      var data = build_response(404, "Short not found", null)
     }
-    res.setHeader('Content-Type', 'application/json')
-    res.send(JSON.stringify(data))
+    respond(res, data)
   })
 });
 
 router.post('/create', function (req, res) {
   if (req.body.url === undefined){
-    res.status(400)
-    var data = {
-        "status" : 400,
-        "result" : {
-          "message" : "Missing url"
-        }
-      }
+    var data = build_response(400, "Missing url", null)
   } else {
     db.create(req.body.url, req.body.short, function(err, creation){
       if (creation) {
-        var data = {
-            "status" : 201,
-            "result" : {
-              "message": "Success!",
-              "url" : creation.url,
-              "short" : creation.short
-            }
-          }
+        var data = build_response(201, "Success!", {"url" : creation.url, "short" : creation.short})
       } else {
-        res.status(400)
-        var data = {
-            "status" : 400,
-            "result" : {
-              "message" : "Failed to create"
-            }
-          }
+        var data = build_response(400, "Failed to create", null)
       }
-      res.setHeader('Content-Type', 'application/json')
-      res.send(JSON.stringify(data))
+      respond(res, data)
     })
   }
 });
